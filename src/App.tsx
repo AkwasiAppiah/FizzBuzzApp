7ecb69940d19a17cde114e4818b551ee0c88cb03
Akwasi Appiah <akwasi.atta.appiah@gmail.com>
2021-07-02 14:48:15 +0100

    Final Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // import { greet } from "./utils/greet";
-import FizzBuzzApp from "./components/FizzBuzzApp"
+import FizzBuzzApp from "./components/FizzBuzzApp";
 
 function App(): JSX.Element {
   return <FizzBuzzApp />;
